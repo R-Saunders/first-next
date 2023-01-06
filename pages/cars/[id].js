5d279db866efa7b65c1cae8f55c99d22f323843c
Rich Saunders <richsaunders@TechEd-0008.broadband>
2023-01-06 12:44:16 +0000

    started to make a dynaic page

---
+++ pages/cars/[id].js
@@ -0,0 +1,8 @@
+import { useRouter, userRouter } from 'next/router'
+
+export default function Car() {
+  const router = useRouter()
+  const { id } = router.query
+
+  return <h1>Hello {id}</h1>
+}
